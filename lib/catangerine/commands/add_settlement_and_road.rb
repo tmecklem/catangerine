6eb08753ddb7d81ab55a020f76494256bef15797
Tim Mecklem <tim@teamgaslight.com>
2014-12-09 23:13:20 -0500

    Refactor and check for round 2 on resource addition

--- lib/catangerine/commands/add_settlement_and_road.rb
+++ lib/catangerine/commands/add_settlement_and_road.rb
@@ -3,26 +3,21 @@ module Catangerine
     class AddSettlementAndRoad
       attr_reader :success
 
-      def initialize(player, q0, r0, v, q1, r1, e, add_resources = false)
-        @q = q0
-        @r = r0
-        @v = v
+      def initialize(player, q0, r0, v, q1, r1, e)
+        @vertex_position = [q0, r0, v]
         @player = player
         @add_settlement = AddSettlement.new(player, q0, r0, v)
         @add_road = AddRoad.new(player, q1, r1, e)
-        @add_resources = add_resources
       end
 
       def execute(game_manager)
         @success = @add_settlement.execute(game_manager) && @add_road.execute(game_manager)
-        if @success
-          resource_me(game_manager)
-        end
+        resource_me(game_manager) if @success && game_manager.round == 2
         @success
       end
 
       def resource_me(game_manager)
-        vertex = game_manager.board.hex_at(@q, @r).vertices[@v]
+        vertex = game_manager.board.vertex_at(*@vertex_position)
         vertex.touching_hexes.each do |hex|
           @player.resource_cards[hex.face.resource_type] ||= 0
           @player.resource_cards[hex.face.resource_type] += 1
