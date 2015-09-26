260b709fedfa16ab5deb371d17dea2bf273b0b16
Tim Mecklem <tim@teamgaslight.com>
2015-09-25 21:01:47 -0400

    That time that you realize that you don't need half of a complicated algorithm because of the way you changed the other half to work.

--- lib/catangerine/observers/longest_road_observer.rb
+++ lib/catangerine/observers/longest_road_observer.rb
@@ -5,7 +5,7 @@ module Observers
     def road_added(board, road, _location)
       player = road.player
       player_roads = board.roads(player)
-      length_of_road = longest_player_road_length(player_roads).length
+      length_of_road = longest_player_road_length(player_roads)
       return unless length_of_road > 4 && length_of_road > (longest_road_length || 0)
       self.longest_road_length = length_of_road
       self.player_with_longest_road_card = player
@@ -14,33 +14,7 @@ module Observers
     private
 
     def longest_player_road_length(player_roads)
-      connected_road_sets(player_roads).max_by(&:length)
-    end
-
-    def connected_road_sets(player_roads)
-      unvisited_segments = player_roads.dup
-      visited_sets = []
-      until unvisited_segments.empty?
-        road = unvisited_segments.shift
-        connected_road = ConnectedRoad.new
-        visited_sets << connected_road
-        visit_road(road, unvisited_segments, connected_road)
-      end
-      visited_sets
-    end
-
-    def visit_road(segment, unvisited_segments, connected_road)
-      connected_road.road_segments << segment
-      unvisited_connected_segments(segment, unvisited_segments).each do |connected_segment|
-        unvisited_segments.delete(connected_segment)
-        visit_road(connected_segment, unvisited_segments, connected_road)
-      end
-    end
-
-    def unvisited_connected_segments(segment, unvisited_segments)
-      segment.connected_segments.map { |connected_segment|
-        connected_segment[:road] if unvisited_segments.include?(connected_segment[:road])
-      }.compact
+      ConnectedRoad.new(road_segments: player_roads).length
     end
   end
 end
