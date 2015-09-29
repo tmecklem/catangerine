f3a9b818ab1b1879e7fa3fd3305741bef1cfebe3
Tim Mecklem <tim@teamgaslight.com>
2015-09-29 00:23:51 -0400

    Use vertex_at directly

--- lib/catangerine/conditions/settlement_and_road_touching.rb
+++ lib/catangerine/conditions/settlement_and_road_touching.rb
@@ -4,8 +4,7 @@ module Catangerine
       road_location = command.attributes[:road_location]
       settlement_location = command.attributes[:settlement_location]
       settlement_vertex = game_manager.board
-                          .hex_at(settlement_location)
-                          .vertices[settlement_location.direction]
+                          .vertex_at(settlement_location)
       protruding_edge_locations = settlement_vertex.protruding_edges.map do |edge|
         Location.new(edge.hex.location.q, edge.hex.location.r, edge.direction)
       end
